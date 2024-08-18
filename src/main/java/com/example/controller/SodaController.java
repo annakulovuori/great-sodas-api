15a42857c4b5a9d83d2aa80fe51de704ab7f1e29
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-08-18 15:26:37 +0300

    FEAT: find by id. ADD: raw error handling

--- src/main/java/com/example/controller/SodaController.java
+++ src/main/java/com/example/controller/SodaController.java
@@ -3,25 +3,32 @@ package com.example.controller;
 import com.example.entity.Soda;
 import com.example.repository.SodaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping(value = "/api/sodas")
 public class SodaController {
 
-    SodaRepository sodarepository;
+    SodaRepository sodaRepository;
     @Autowired
-    public SodaController(SodaRepository sodarepository) {
-        this.sodarepository = sodarepository;
+    public SodaController(SodaRepository sodaRepository) {
+        this.sodaRepository = sodaRepository;
     }
 
     @GetMapping(value = "/api/sodas/{id}")
     public ResponseEntity<Soda> fetchById(@PathVariable int id) {
-        return null;
+        Optional<Soda> optionalSoda =  sodaRepository.findById(id);
+        if (optionalSoda.isEmpty()) {
+            throw new IllegalArgumentException("cannot find with id");
+        }
+        return new ResponseEntity<Soda>(optionalSoda.get(), HttpStatus.OK);
     }
 
 }
