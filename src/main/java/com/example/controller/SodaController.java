8cc7533d1043aa528a888a463d703dfdaab4b2f7
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-08-18 14:52:20 +0300

    CREATE: new method fetchById with id path variable

--- src/main/java/com/example/controller/SodaController.java
+++ src/main/java/com/example/controller/SodaController.java
@@ -1,7 +1,19 @@
 package com.example.controller;
 
+import com.example.entity.Soda;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/api/sodas")
 public class SodaController {
+
+    @GetMapping(value = "/api/sodas/{id}")
+    public ResponseEntity<Soda> fetchById(@PathVariable int id) {
+        return null;
+    }
+
 }
