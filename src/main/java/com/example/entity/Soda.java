4bc62614bd2e7c0bac683f0ba5b25f27612b91c0
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-08-18 15:39:29 +0300

    ADD: table name

--- src/main/java/com/example/entity/Soda.java
+++ src/main/java/com/example/entity/Soda.java
@@ -3,6 +3,7 @@ package com.example.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "sodas")
 public class Soda {
     @Id
     @GeneratedValue
