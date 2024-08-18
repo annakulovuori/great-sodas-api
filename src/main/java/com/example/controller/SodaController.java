4881f3ac743205f092a1103cd81be1efcf43193d
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-08-18 15:11:02 +0300

    ADD: autowired constructor injecting SodaRepository

--- src/main/java/com/example/controller/SodaController.java
+++ src/main/java/com/example/controller/SodaController.java
@@ -1,6 +1,8 @@
 package com.example.controller;
 
 import com.example.entity.Soda;
+import com.example.repository.SodaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/api/sodas")
 public class SodaController {
+
+    SodaRepository sodarepository;
+    @Autowired
+    public SodaController(SodaRepository sodarepository) {
+        this.sodarepository = sodarepository;
+    }
 
     @GetMapping(value = "/api/sodas/{id}")
     public ResponseEntity<Soda> fetchById(@PathVariable int id) {
