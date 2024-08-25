9315a323b064872b4ec5667cb8275535bfb8eaff
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-08-25 15:55:00 +0300

    FEAT: Handle MethodArgumentTypeMismatchExceptions and return responseEntity with error message(using ErrorInfo class) and status code

--- src/main/java/com/example/exception/MyExceptionHandler.java
+++ src/main/java/com/example/exception/MyExceptionHandler.java
@@ -2,8 +2,10 @@ package com.example.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 
 @ControllerAdvice
@@ -18,5 +20,11 @@ public class MyExceptionHandler {
     public ResponseEntity<ErrorInfo> handleConflict(Exception ex) {
         ErrorInfo e = new ErrorInfo("Something went wrong");
         return new ResponseEntity<ErrorInfo>(e, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorInfo> handleConflict(MethodArgumentTypeMismatchException ex) {
+        ErrorInfo e = new ErrorInfo("Argument type doesn't match");
+        return new ResponseEntity<ErrorInfo>(e, HttpStatus.BAD_REQUEST);
     }
 }
