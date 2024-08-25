76b35dcc2e44211856674090b366b3eb2ae6595f
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-08-25 15:56:24 +0300

    REFACTOR: change method order

--- src/main/java/com/example/exception/MyExceptionHandler.java
+++ src/main/java/com/example/exception/MyExceptionHandler.java
@@ -16,15 +16,16 @@ public class MyExceptionHandler {
         return new ResponseEntity<ErrorInfo>(e, HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorInfo> handleConflict(MethodArgumentTypeMismatchException ex) {
+        ErrorInfo e = new ErrorInfo("Argument type doesn't match");
+        return new ResponseEntity<ErrorInfo>(e, HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorInfo> handleConflict(Exception ex) {
         ErrorInfo e = new ErrorInfo("Something went wrong");
         return new ResponseEntity<ErrorInfo>(e, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ResponseEntity<ErrorInfo> handleConflict(MethodArgumentTypeMismatchException ex) {
-        ErrorInfo e = new ErrorInfo("Argument type doesn't match");
-        return new ResponseEntity<ErrorInfo>(e, HttpStatus.BAD_REQUEST);
-    }
 }
