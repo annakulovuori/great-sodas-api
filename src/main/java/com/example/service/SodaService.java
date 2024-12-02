200009a831b2f634f282c7f3722ec68c4a809a70
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-12-02 11:22:33 +0200

    CREATE: new method updateById

--- src/main/java/com/example/service/SodaService.java
+++ src/main/java/com/example/service/SodaService.java
@@ -1,2 +1,16 @@
-package com.example.service;public class SodaService {
+package com.example.service;
+
+import com.example.entity.Soda;
+import com.example.repository.SodaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SodaService {
+
+    private final SodaRepository repository;
+
+    public Soda updateById(int id, Soda soda) {
+    }
 }
